0354ca52eefbb4f4465c6efa01885dfbdad3cf10
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-01-11 19:23:36 -0300

    chore: Added frontend

--- frontend/br_med_chart/src/App.js
+++ frontend/br_med_chart/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const options = {
+  title: {
+    text: "Currencies",
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <navbar className="navbar">
+        <h3>Elias Challenge</h3>
+      </navbar>
+      <div className="body-container">
+        <div className="left-sidebar">
+          <h3 className="title">Consultas</h3>
+        </div>
+        <div className="chart-info-holder">
+        <div className="high-chart">
+          <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
