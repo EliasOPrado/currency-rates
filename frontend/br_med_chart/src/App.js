ec375d2ca1dab0c9722d3d4096a61b691d6fdaa6
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-01-12 20:54:11 -0300

    chore: improved layout and added spinner

--- frontend/br_med_chart/src/App.js
+++ frontend/br_med_chart/src/App.js
@@ -5,23 +5,30 @@ import HighchartsReact from "highcharts-react-official";
 
 function App() {
   const [endDate, setEndDate] = useState('');
+  const [loading, setLoading] = useState(false);
   const [startDate, setStartDate] = useState('');
   const [exchangeData, setExchangeData] = useState([]);
   const [targetCurrency, setTargetCurrency] = useState('BRL');
 
   useEffect(() => {
     fetchData();
-  }, [targetCurrency, endDate]);
+  }, [targetCurrency, startDate, endDate]);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/currency-rates/?start_date=${startDate}&end_date=${endDate}&target_currency=${targetCurrency}`
-      );
-      const data = await response.json();
-      setExchangeData(data);
+      setLoading(true); // Set loading to true when starting the request
+
+      if (startDate && endDate) {
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/currency-rates/?start_date=${startDate}&end_date=${endDate}&target_currency=${targetCurrency}`
+        );
+        const data = await response.json();
+        setExchangeData(data);
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false); // Set loading to false after the request, whether it succeeds or fails
     }
   };
 
@@ -33,15 +40,17 @@ function App() {
     setStartDate(event.target.value);
   };
 
-  console.log("END DATE ----->", endDate, startDate)
-
   const handleEndDateChange = (event) => {
     setEndDate(event.target.value);
   };
 
   const options = {
     chart: {
-      type: 'line', 
+      type: 'line',
+      borderColor: '#ccc',
+      borderRadius: 10,
+      borderWidth: 2,
+      padding:500
     },
     title: {
       text: 'Exchange Rate Data',
@@ -83,19 +92,20 @@ function App() {
             <div className="section ">
               <h3 className="title">Moedas</h3>
               <div className="buttons">
-                <button className="button-design" onClick={() => handleButtonClick('BRL')}>BRL</button>
-                <button className="button-design" onClick={() => handleButtonClick("EUR")}>EUR</button>
-                <button className="button-design" onClick={() => handleButtonClick("JPY")}>JPY</button>
+                <button className="button-design" onClick={() => handleButtonClick('BRL')} disabled={loading}>BRL</button>
+                <button className="button-design" onClick={() => handleButtonClick("EUR")} disabled={loading}>EUR</button>
+                <button className="button-design" onClick={() => handleButtonClick("JPY")} disabled={loading}>JPY</button>
                 <div>
                 <label>Start Date:</label>
-                <input type="date" value={startDate} onChange={handleStartDateChange} max={new Date().toISOString().split('T')[0]} />
+                <input type="date" className="calendar" value={startDate} onChange={handleStartDateChange} max={new Date().toISOString().split('T')[0]} />
         
                 <label>End Date:</label>
-                <input type="date" value={endDate} onChange={handleEndDateChange} max={new Date().toISOString().split('T')[0]} />
+                <input type="date" className="calendar" value={endDate} onChange={handleEndDateChange} max={new Date().toISOString().split('T')[0]} />
               </div>
               </div>
             </div>
           </div>
+          {loading && <div className="loading-spinner">Loading...</div>}
           <div className="high-chart">
             <HighchartsReact highcharts={Highcharts} options={options} />
           </div>
